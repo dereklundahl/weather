4cccb2e21001f72520f3c285604857a3e2c42ecf
Derek Lundahl <dereklundahl@yahoo.com>
2019-03-22 19:54:21 -0600

    app functions but temp is off. Will investigate when understand better how to use dev tools to see the data

--- App.js
+++ App.js
@@ -6,36 +6,120 @@ import {
   Text,
   KeyboardAvoidingView,
   Platform,
+  ActivityIndicator,
+  StatusBar,
 } from 'react-native';
 
+import { fetchLocationId, fetchWeather } from './utils/api';
 import getImageForWeather from './utils/getImageForWeather';
 
 import SearchInput from './components/SearchInput';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      error: false,
+      location: '',
+      temperatrure: 0,
+      weather: '',
+    };
+  }
+
+  componentDidMount() {
+    this.handleUpdateLocation('San Francisco');
+  }
+
+  handleUpdateLocation = async city => {
+    if(!city) return;
+
+    this.setState({ loading: true }, async () => {
+      try {
+        const locationId = await fetchLocationId(city);
+        const { location, weather, temperature } = await fetchWeather(
+          locationId,
+        );
+
+        this.setState({
+          loading: false,
+          error: false,
+          location,
+          weather,
+          temperature,
+        });
+      } catch(e) {
+        this.setState({
+          loading: false,
+          error: true,
+        });
+      }
+    });
+  };
+
   render() {
+    const { 
+      loading,
+      error,
+      location,
+      weather,
+      temperature,
+     } = this.state;
+    
     return (
       <KeyboardAvoidingView
         style={styles.container}
         behavior="padding"
       >
+
+        <StatusBar barStyle="light-content" />
         <ImageBackground
-          source={getImageForWeather('Clear')}
+          source={getImageForWeather(weather)}
           style={styles.imageContainer}
           imageStyle={styles.image}
         >
           <View style={styles.detailsContainer}>
-            <Text style={[styles.largeText, styles.textStyle]}>
-              San Francisco
-            </Text>
-            <Text style={[styles.smallText, styles.textStyle]}>
-              Light Cloud
-            </Text>
-            <Text style={[styles.largeText, styles.textStyle]}>
-              24°
-            </Text>
 
-            <SearchInput placeholder="Search any city" />
+            <ActivityIndicator
+              animating={loading}
+              color="white"
+              size="large"
+            />
+
+            {!loading && (
+              <View>
+                {error && (
+                  <Text style={[styles.smallText, styles.textStyle]}>
+                    Could not load weather, please try a different city.
+                  </Text>
+                )}
+
+                {!error && (
+                  <View>
+                    <Text
+                      style={[styles.largeText, styles.textStyle]}
+                    >
+                      {location}
+                    </Text>
+                    <Text
+                      style={[styles.smallText, styles.textStyle]}
+                    >
+                      {weather}
+                    </Text>
+                    <Text 
+                      style={[styles.largeText, styles.textStyle]}
+                    >
+                      {`${Math.round(temperature)}°`}
+                    </Text>
+                  </View>
+                )}
+
+                <SearchInput 
+                    placeholder="Search any city" 
+                    onSubmit={this.handleUpdateLocation}
+                />
+              </View>
+            )}
           </View>
         </ImageBackground>
       </KeyboardAvoidingView>
